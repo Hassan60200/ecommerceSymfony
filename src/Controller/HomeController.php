49c3de83abfb9687967cb07ffa7e7f378671a4bb
Hassan Derkaoui <derkaouihassan19@gmail.com>
2020-11-23 16:26:09 +0100

    upgrade homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,9 +14,15 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home_page")
      */
-    public function index(EntityManagerInterface $em):Response
+    public function index(EntityManagerInterface $em, ProductRepository $repo):Response
     {
+        $products = $repo->findBy([], [], 3);
 
-        return $this->render('home/index.html.twig',);
+
+        return $this->render('home/index.html.twig',
+        [
+            'products' => $products
+        ]
+        );
     }
 }
