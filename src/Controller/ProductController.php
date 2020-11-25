0cd9af2d6456079579901d24ff2cf7c77203583d
Hassan Derkaoui <derkaouihassan19@gmail.com>
2020-11-25 16:36:43 +0100

    create form edit for product

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/{slug}", name="product_category")
+     */
+    public function category($slug, CategoryRepository $categoryRepo): Response
+    {
+        $category = $categoryRepo->findOneBy([
+            'slug' => $slug
+        ]);
+        if (!$category) {
+           throw $this->createNotFoundException("La catégorie demandée n'existe pas");
+        }
+        return $this->render('product/category.html.twig', [
+            'slug' => $slug,
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * @Route("/{category_slug}/{slug}", name="product_show")
+     */
+    public function show($slug, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->findOneBy([
+            'slug' => $slug
+        ]);
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit demandée n'existe pas");
+        }
+        return $this->render('product/show.html.twig', [
+            'slug' => $slug,
+            'product' => $product
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/create", name="product_create")
+     */
+    public function create(Request $request, EntityManagerInterface $em, SluggerInterface $slugger){
+
+        $product = new Product;
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $product->setSlug(strtolower($slugger->slug($product->getName())));
+
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('product_show', [
+                'category_slug' => $product->getCategory()->getSlug(),
+                'slug' => $product->getSlug()
+            ]);
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/{id}/edit", name="product_edit")
+     */
+    public function edit(ProductRepository $productRepo, $id, Request $request, EntityManagerInterface $em){
+
+        $product = $productRepo->find($id);
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $em->flush();
+
+            return $this->redirectToRoute('product_show', [
+                'category_slug' => $product->getCategory()->getSlug(),
+                'slug' => $product->getSlug()
+            ]);
+        }
+
+        $formView = $form->createView();
+
+
+        return $this->render('product/edit.html.twig',[
+            'product' => $product,
+            'formView' => $formView
+        ]);
+
+    }
+}
