72b8f6a5f6abb3cf85b98024e8476808206ec083
Hassan Derkaoui <derkaouihassan19@gmail.com>
2020-12-04 15:49:31 +0100

    create function login an logout

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,20 +3,25 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\User;
 use Faker\Factory;
 use App\Entity\Product;
 use Liior\Faker\Prices;
 use Bezhanov\Faker\Provider\Commerce;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
     protected $slugger;
+    protected $encoder;
 
-    public function __construct(SluggerInterface $slugger){
+    public function __construct(SluggerInterface $slugger, UserPasswordEncoderInterface $encoder)
+    {
         $this->slugger = $slugger;
+        $this->encoder = $encoder;
     }
 
     public function load(ObjectManager $manager)
@@ -26,26 +31,49 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
 
-        for ($c = 0 ; $c < 3; $c++){
+        $admin = new User();
+
+        $hash = $this->encoder->encodePassword($admin, "password");
+
+        $admin->setEmail("admin@gmail.com")
+            ->setFullName("Admin")
+            ->setPassword($hash)
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User();
+
+            $hash = $this->encoder->encodePassword($user, "password");
+
+            $user->setEmail("user$u@gmail.com")
+                ->setFullName($faker->name)
+                ->setPassword($hash);
+
+            $manager->persist($user);
+        }
+
+        for ($c = 0; $c < 3; $c++) {
             $category = new Category();
             $category->setName($faker->department)
                 ->setSlug(strtolower($this->slugger->slug($category->getName())));
 
             $manager->persist($category);
 
-            for ($p = 0 ; $p < mt_rand(15, 20); $p++){
+            for ($p = 0; $p < mt_rand(15, 20); $p++) {
                 $product = new Product();
                 $product->setName($faker->productName)
                     ->setPrice($faker->price(4000, 20000))
                     ->setSlug(strtolower($this->slugger->slug($product->getName())))
                     ->setCategory($category)
-                ->setShortDescription($faker->paragraph())
-                ->setPicture($faker->imageUrl(400, 400, true));
+                    ->setShortDescription($faker->paragraph())
+                    ->setPicture($faker->imageUrl(400, 400, true));
 
                 $manager->persist($product);
             }
         }
-
 
 
         $manager->flush();
