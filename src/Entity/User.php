6fb576c198674894f27625ebca673c2df3332e20
Hassan Derkaoui <derkaouihassan19@gmail.com>
2020-12-19 17:51:27 +0100

    add registration form and customize template mailer

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -22,6 +25,8 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\NotBlank(message="L'adresse mail est obligatoire")
+     * @Assert\Email(message="Veuillez renseigné un email valide !")
      */
     private $email;
 
@@ -33,11 +38,17 @@ class User implements UserInterface
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Assert\Length(min = 6,max = 50, minMessage = "Votre mot de passe doit faire au moins {{ limit }} caractères", maxMessage = "Votre mot de passe ne doit pas dépasser {{ limit }} caractères")
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="L'adresse mail est obligatoire")
+     * @Assert\Length(min =6,
+    max = 50,
+    minMessage = "Votre nom complet doit faire au moins 6 caractères",
+    maxMessage = "Votre nom complet ne doit pas dépasser 50 caractères")
      */
     private $fullName;
 
@@ -50,6 +61,11 @@ class User implements UserInterface
      * @ORM\OneToMany(targetEntity=Purchase::class, mappedBy="user")
      */
     private $purchases;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -81,7 +97,7 @@ class User implements UserInterface
      */
     public function getUsername(): string
     {
-        return (string) $this->email;
+        return (string)$this->email;
     }
 
     /**
@@ -108,7 +124,7 @@ class User implements UserInterface
      */
     public function getPassword(): string
     {
-        return (string) $this->password;
+        return (string)$this->password;
     }
 
     public function setPassword(string $password): self
@@ -203,6 +219,18 @@ class User implements UserInterface
                 $purchase->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
